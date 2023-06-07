a963119b3cd9396923c5170e3d2ea338436e8904
TGarcia <tgarciamenese@udesa.edu.ar>
2023-06-06 21:19:47 -0300

    genres.html y genres.js

---
+++ js/genres.js
@@ -0,0 +1,30 @@
+let genresUrl = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre';
+
+fetch(genresUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        let genresList = document.querySelector('.genres-list');
+        let genresHTML = '';
+
+        for (let i = 0; i < data.data.length; i++) {
+            let genre = data.data[i];
+            genresHTML += `
+                <li class="lista-genre">
+                    <a href="detail-genres.html?id=${genre.id}">
+                    <img class"imagen-genres" src="${genre.picture_medium}" alt="${genre.name}">
+                        <h1 class='genre-h1'>${genre.name}</h1>
+                        
+                    </a>
+                </li>
+            `;
+        }
+
+        genresList.innerHTML = genresHTML;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
