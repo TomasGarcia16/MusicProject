d6b46a9405249d6431d93930b3a6d8f700a91d27
TGarcia <tgarciamenese@udesa.edu.ar>
2023-06-14 14:55:58 -0300

    Datail- artist

--- js/detail-artistas.js
+++ js/detail-artistas.js
@@ -1,5 +1,5 @@
 let urlDetalles = new URLSearchParams(window.location.search);
-let dataId = urlDetalles.get("id"); //querystring
+let dataId = urlDetalles.get("id"); // querystring
 
 let detailUrl = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${dataId}`;
 
@@ -8,14 +8,29 @@ fetch(detailUrl)
     return response.json();
   })
   .then(function(data) {
-    console.log(data)
+    console.log(data);
     let artistName = data.name;
     let fotoArtista = data.picture_medium;
-//.textcontent=funciona como innerHtml o innerText
     document.querySelector(".artistName").textContent = artistName;
     document.querySelector(".artistFoto").setAttribute("src", fotoArtista);
 
-})
-.catch(function(error) {
-  console.log("error:" + error);
-});
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${dataId}/top`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data)
+        let htmlTrackList = "";
+        let array = data.data;
+        for (let i = 0; i < array.length; i++) {
+          htmlTrackList += `<li class="canciones"><a class="cancionest" href="detail-album.html?id=${array[i].album.id}">${array[i].album.title}</a></li>`;
+        }
+        document.querySelector(".artistAlbums").innerHTML = htmlTrackList;
+      })
+      .catch(function(error) {
+        console.log("error: " + error);
+      });
+  })
+  .catch(function(error) {
+    console.log("error: " + error);
+  });
